848b8d3ab0098558a5592a781b69e80a8ab85d1c
akane-5 <cocoa.wots7@gmail.com>
2024-07-07 16:34:51 +0900

    トップページの再作成、TailwindCSSとdaisyUIの導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,22 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'green-100': '#e6f4e6',
+        'green-700': '#3b5a3b',
+        'green-50': '#f2f9f2',
+      },
+    },
+  },
+  plugins: [
+    require('daisyui')
+  ],
+  daisyui: {
+    themes: [
+      "bumblebee"
+    ],
+  },
 }
