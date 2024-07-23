7bdea42b66549652d55effc767f414e4e08df7dd
akane-5 <cocoa.wots7@gmail.com>
2024-07-24 01:19:13 +0900

    Add: 検索結果一覧ページの作成

--- app/controllers/shrines_controller.rb
+++ app/controllers/shrines_controller.rb
@@ -1,6 +1,10 @@
 class ShrinesController < ApplicationController
+  def search
+    @q = Shrine.ransack(params[:q])
+  end
+
   def index
     @q = Shrine.ransack(params[:q])
-    @shrines = @q.result.includes(:prefecture)
+    @shrines = @q.result
   end
 end
